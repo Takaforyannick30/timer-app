7a3802212450d63d73d4dc12ed08370072e2e027
Takaforyannick30 <takaforyannick30@gmail.com>
2021-04-18 05:13:21 +0200

    Added styling for Timer Component

--- src/Timer.js
+++ src/Timer.js
@@ -3,9 +3,9 @@
 function Timer() {
     return(
         <div>
-            <p>You have been on this site for:</p>
+            <p className="timer-p">You have been on this site for:</p>
             <span>50.20</span>
-            <p>Seconds</p>
+            <p className="timer-p">Seconds</p>
 
         </div>
     );
